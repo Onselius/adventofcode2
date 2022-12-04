e3a88b376ed2435aa2b67cc14597d597fd8e62b7
lars <lars.onselius@gmail.com>
2022-12-04 21:24:20 +0100

    2022-12-04

--- src/main/java/year22/Dec04.java
+++ src/main/java/year22/Dec04.java
@@ -1,5 +1,6 @@
 package year22;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Dec04 {
@@ -10,13 +11,50 @@ public class Dec04 {
 
     protected int part1()
     {
-        return 1;
+        int count = 0;
+        for (String line: lines)
+        {
+            String[] split = line.split(",");
+            int[] a = Arrays.stream(split[0].split("-")).mapToInt(Integer::valueOf).toArray();
+            int[] b = Arrays.stream(split[1].split("-")).mapToInt(Integer::valueOf).toArray();
+
+            if (a[0] <= b[0] && a[1] >= b[1])
+            {
+                count++;
+            }
+            else if (b[0] <= a[0] && b[1] >= a[1])
+            {
+                count++;
+            }
+        }
+        return count;
     }
 
     protected int part2()
     {
+        int count = 0;
+        for (String line: lines)
+        {
+            String[] split = line.split(",");
+            int[] a = Arrays.stream(split[0].split("-")).mapToInt(Integer::valueOf).toArray();
+            int[] b = Arrays.stream(split[1].split("-")).mapToInt(Integer::valueOf).toArray();
 
-        return 1;
+            System.out.println(line);
+            if (a[0] <= b[0] && a[1] >= b[0])
+            {
+                count++;
+            }
+            else if (a[0] <= b[1] && a[1] >= b[1])
+            {
+                count++;
+            }
+            else if (a[0] >= b[0] && a[1] <= b[1])
+            {
+                count++;
+            }
+            System.out.println(count);
+        }
+        return count;
     }
 
     public void setLines(List<String> lines)
